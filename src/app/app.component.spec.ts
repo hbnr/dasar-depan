528a21766a42b20a45df5206031261ce83ba99ff
Sammy <smmy@hbnr.nl>
2023-10-22 00:14:20 +0200

    replaced jasmine/karma for jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hbnr-dasar-depan');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'hbnr-dasar-depan app is running!',
-    );
-  });
 });
